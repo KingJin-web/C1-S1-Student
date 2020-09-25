273d2ab47365e2417071e4656c5e1ec91fd74ff2
huahuaz <824276226@qq.com>
2020-09-25 14:06:19 +0800

    老师主页面，留言板

--- src/dao/StuDao.java
+++ src/dao/StuDao.java
@@ -1,5 +1,26 @@
 package dao;
 
+import java.util.List;
+import java.util.Map;
+
+import bean.Message;
+import util.DBHelper;
+
 public class StuDao {
-	
+	public String showMessage() {
+		DBHelper dbh = new DBHelper();
+		String sql = "select sname,mdate,message from messageboard order by Mdate desc";
+		List<Message> list = dbh.query(sql,Message.class);
+		String ms = "学生留言：";
+		for(Message mss : list) {
+			ms = ms + String.valueOf(mss);
+		}
+//		String ms = String.valueOf(list);
+		System.out.println(ms);
+		return ms;
+	}
+	public static void main(String[] args) {
+		StuDao s = new StuDao();
+		s.showMessage();
+	}
 }
